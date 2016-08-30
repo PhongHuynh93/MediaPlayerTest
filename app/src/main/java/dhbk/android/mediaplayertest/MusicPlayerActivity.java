5ab1ef00e1d655c7aeb0c3d2fab87f9dceab8256
Phong <p7157568@gmail.com>
2016-08-30 18:55:53 +0700

    feat - get String form voice via Intent

--- app/src/main/java/dhbk/android/mediaplayertest/MusicPlayerActivity.java
+++ app/src/main/java/dhbk/android/mediaplayertest/MusicPlayerActivity.java
@@ -1,5 +1,8 @@
 package dhbk.android.mediaplayertest;
 
+import android.app.SearchManager;
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,18 +12,46 @@ import android.os.Bundle;
  * connected while this activity is running.
  */
 public class MusicPlayerActivity extends AppCompatActivity {
+    private static final String SAVED_MEDIA_ID="com.example.android.uamp.MEDIA_ID";
+
+    private Bundle mVoiceSearchParams;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
 
-
         // declare toolbar
         initializeToolbar();
+
+        // todo declare param from intent
+        initializeFromParams(savedInstanceState, getIntent());
+
     }
 
     protected void initializeToolbar() {
 
+    }
+
+    /**
+     * todo 2 get the string from voice
+     * @param savedInstanceState
+     * @param intent
+     */
+    protected void initializeFromParams(Bundle savedInstanceState, Intent intent) {
+        String mediaId = null;
+        // check if we were started from a "Play XYZ" voice search. If so, we save the extras
+        // (which contain the query details) in a parameter, so we can reuse it later, when the
+        // MediaSession is connected.
+        if (intent.getAction() != null && intent.getAction().equals(MediaStore.INTENT_ACTION_MEDIA_PLAY_FROM_SEARCH)) {
+            mVoiceSearchParams = intent.getExtras();
+        } else {
+            if (savedInstanceState != null) {
+                // If there is a saved media ID, use it
+                mediaId = savedInstanceState.getString(SAVED_MEDIA_ID);
+            }
+        }
+
+        navigateToBrowser(mediaId);
     }
 }
